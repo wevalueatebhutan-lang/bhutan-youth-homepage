806794bf46fe6d3d5bc797efaba87bcf46f3fd7d
Bhutan Youth Admin <wevalueatebhutan@gmail.com>
2026-08-10 01:40:09 +0900

    style: apply Freesentation webfont to entire website

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: 'bhutantaekwondo.org',
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
